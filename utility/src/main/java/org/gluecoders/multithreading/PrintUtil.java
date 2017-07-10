7ec14ccf595e849ea3f14a31b2bf08627c126720
anandrajneesh@outlook.com <anand_rajneesh@infosys.com>
2017-07-10 11:36:39 +0530

    [Utility] utility module for common helpers

---
+++ utility/src/main/java/org/gluecoders/multithreading/PrintUtil.java
@@ -0,0 +1,11 @@
+package org.gluecoders.multithreading;
+
+/**
+ * Created by Anand_Rajneesh on 6/26/2017.
+ */
+public class PrintUtil {
+
+    public static void linebreak(){
+        System.out.println();
+    }
+}
